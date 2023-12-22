2db0f783c2853882941c08b31356d52ecbe12eaa
Luciana Rocha <lucianajrocha97@gmail.com>
2023-12-22 14:45:47 -0300

    implementando variavies e lacos

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,33 @@
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        int opcao;
+        do {
+            System.out.println("*****************************************************");
+            System.out.println("Menu principal: ");
+            System.out.println("1 - Acessar menu de vendedor");
+            System.out.println("2 - Acessar menu de comprador");
+            System.out.println("*****************************************************");
+            opcao = leia.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Acessando menu de vendedor...");
+                    break;
+                case 2:
+                    System.out.println("Acessando menu de comprador...");
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        } while (opcao != 0);
     }
 }
+
